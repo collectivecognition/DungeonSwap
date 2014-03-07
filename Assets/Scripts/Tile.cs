76bd01f1358230b3dcd66ad1a6877ece74bb21dc
Will Blanchette <wb@Wills-iMac.local>
2014-03-07 10:52:01 -0500

    Refactored rendering and dragging code

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,48 +4,12 @@ using System.Collections;
 public class Tile : MonoBehaviour {
 
 	public Sprite[] sprites;
-
-	private enum DragDir {None, Horizontal, Vertical};
-
+	
 	private Sprite sprite;
-	private bool dragging = false;
-	private DragDir draggingDir = DragDir.None;
-	private Vector3 draggingPos;
 
 	// Use this for initialization
 	void Start () {
 		sprite = sprites [Random.Range (0, sprites.Length)];
 		this.GetComponent<SpriteRenderer> ().sprite = sprite;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(dragging){
-			Vector2 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
-			float diff = Vector3.Distance(mousePos, draggingPos);
-
-			if(draggingDir == DragDir.None && diff > 0){
-				draggingDir = Mathf.Abs(mousePos.x - draggingPos.x) > Mathf.Abs (mousePos.y - draggingPos.y) ? DragDir.Horizontal : DragDir.Vertical;
-			}
-		}
-	}
-
-	void OnMouseDown () {
-		dragging = true;
-		draggingPos = transform.position;
-		Debug.Log ("Dragging");
-	}
-
-	void OnMouseUp () {
-		dragging = false;
-		draggingDir = DragDir.None;
-		Debug.Log ("Not Dragging");
-	}
-
-	void OnMouseDrag () {
-		if (dragging) {
-			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
-			Debug.Log (mousePos - draggingPos);
-		}
 	}
 }
--- Assets/Scripts/Tiles.cs
+++ Assets/Scripts/Tiles.cs
@@ -9,21 +9,86 @@ public class Tiles : MonoBehaviour {
 	private int tileH = 7;
 	private Transform[,] tiles;
 
+	private enum DragDir {None, Horizontal, Vertical};
+	private bool dragging = false;
+	private DragDir draggingDir = DragDir.None;
+	private int draggingRow;
+	private int draggingCol;
+	private Vector3 draggingPos;
+
 	// Use this for initialization
+
 	void Start () {
 		tiles = new Transform[tileW, tileH];
 
 		for(int ii = 0; ii < tileW; ii++){
 			for(int jj = 0; jj < tileH; jj++){
 				Transform tile = (Transform)Instantiate(tilePrefab);
-				tile.transform.position = new Vector3(ii, jj, 0f);
+				tile.parent = this.transform;
+				tile.transform.localPosition = new Vector3(ii - tileW / 2, jj - tileH / 2, 0f);
 				tiles[ii, jj] = tile;
 			}
 		}
 	}
 	
 	// Update is called once per frame
+
 	void Update () {
+		if(dragging){
+			Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+			float diff = Vector3.Distance(mouseWorldPos, draggingPos);
+			float diffX = mouseWorldPos.x - draggingPos.x;
+			float diffY = mouseWorldPos.y - draggingPos.y;
+
+			// Determine which direction we're dragging
+
+			if(draggingDir == DragDir.None && diff > 0.1f){
+				draggingDir = Mathf.Abs(diffX) > Mathf.Abs(diffY) ? DragDir.Horizontal : DragDir.Vertical;
+
+				// Determine which row or column is being dragged
+				
+				draggingCol = -(int)(transform.position.x - mouseWorldPos.x - tileW / 2);
+				draggingRow = -(int)(transform.position.y - mouseWorldPos.y - tileH / 2);
+			}
+
+			// Apply offset to row / column
+			
+			switch(draggingDir){
+				case DragDir.Horizontal:
+					// Set offset on each tile in row
+					
+					for(int ii = 0; ii < tileW; ii++){
+						Transform tile = tiles[ii, draggingRow];
+						tile.localPosition = new Vector3(ii - tileW / 2 + diffX, tile.localPosition.y, tile.localPosition.z);
+					}
+					break;
+				
+				case DragDir.Vertical:
+					// Set offset on each tile in column
+					
+					for(int ii = 0; ii < tileH; ii++){
+						Transform tile = tiles[draggingCol, ii];
+						tile.localPosition = new Vector3(tile.localPosition.x, ii - tileH / 2 + diffY, tile.localPosition.z);
+					}	
+					break;
+			}
+		}
+	}
 	
+	void OnMouseDown () {
+		dragging = true;
+		draggingPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+	
+	void OnMouseUp () {
+		dragging = false;
+		draggingDir = DragDir.None;
+	}
+	
+	void OnMouseDrag () {
+		if (dragging) {
+
+		}
 	}
 }
