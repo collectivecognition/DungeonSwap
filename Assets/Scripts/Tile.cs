9d0c77574170cc3bb9e153aad71ccc831997ee4f
Will Blanchette <wb@Wills-iMac.local>
2014-03-07 15:08:00 -0500

    Added random wall generation

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -3,15 +3,28 @@ using System.Collections;
 
 public class Tile : MonoBehaviour {
 
-	public Sprite[] sprites;
-	
 	private Sprite sprite;
+	public Transform left;
+	public Transform right;
+	public Transform up;
+	public Transform down;
+	public Transform floor;
+
+	Transform Add (Transform what, int layer = 0, float rotation = 0f){
+		Transform thing = (Transform)Instantiate (what);
+		thing.parent = transform;
+		thing.transform.localPosition = new Vector3(0, 0, -layer);
+		thing.transform.Rotate(new Vector3(0, 0, 1), rotation);
+		return thing;
+	}
 
 	// Use this for initialization
 
 	void Start () {
-		sprite = sprites [Random.Range (0, sprites.Length)];
-		this.GetComponent<SpriteRenderer> ().sprite = sprite;
-		this.transform.Rotate (new Vector3 (0, 0, 1), Random.Range (0, 3) * 90f);
+		floor = Add (floor);
+		left = Random.value >= 0.5 ? Add (left, 1) : null;
+		right = Random.value >= 0.5 ? Add (right, 1, 180) : null;
+		up = Random.value >= 0.5 ? Add (up, 1, 90) : null;
+		down = Random.value >= 0.5 ? Add (down, 1, 270) : null;
 	}
 }
