fa5b7f7f45e6e6597f58b176d45ae361b8f43d9d
William Blanchette <wb@collectivecognition.com>
2014-03-10 09:18:49 -0400

    Added basic monster prefab and renamed prefab vars in tile script

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,11 +6,13 @@ public class Tile : MonoBehaviour {
 	private Sprite sprite;
 	
 
-	public Transform floor;
-	public Transform wall;
-	public Transform door;
-	public Transform chest;
+	public Transform floorPrefab;
+	public Transform wallPrefab;
+	public Transform doorPrefab;
+	public Transform chestPrefab;
+	public Transform monsterPrefab;
 
+	public Transform floor;
 	public Transform up;
 	public Transform down;
 	public Transform left;
@@ -30,55 +32,55 @@ public class Tile : MonoBehaviour {
 	// Use this for initialization
 
 	void Start () {
-		floor = Add (floor);
+		floor = Add (floorPrefab);
 
 		switch(Random.Range (0, 4)){
 			case 0:
-				up = Add (door, 1, 90);
+				up = Add (doorPrefab, 1, 90);
 			break;
 
 			case 1:
-				down = Add (door, 1, 270);
+				down = Add (doorPrefab, 1, 270);
 			break;
 
 			case 2:
-				left = Add (door, 1, 0);
+				left = Add (doorPrefab, 1, 0);
 			break;
 
 			case 3:
-				right = Add (door, 1, 180);
+				right = Add (doorPrefab, 1, 180);
 			break;
 		};
 
 		switch(Random.Range (0, 4)){
 			case 0:
 				if(up == null)
-					up = Add (door, 1, 90);
+					up = Add (doorPrefab, 1, 90);
 			break;
 
 			case 1:
 				if(down == null)
-					down = Add (door, 1, 270);
+					down = Add (doorPrefab, 1, 270);
 			break;
 
 			case 2:
 				if(left == null)
-					left = Add (door, 1, 0);
+					left = Add (doorPrefab, 1, 0);
 			break;
 
 			case 3:
 				if(right == null)
-					right = Add (door, 1, 180);
+					right = Add (doorPrefab, 1, 180);
 			break;
 		};
 
-		if(up == null) up = Add (wall, 1, 90);
-		if(down == null) down = Add (wall, 1, 270);
-		if(left == null) left = Add (wall, 1, 0);
-		if(right == null) right = Add (wall, 1, 180);
+		if(up == null) up = Add (wallPrefab, 1, 90);
+		if(down == null) down = Add (wallPrefab, 1, 270);
+		if(left == null) left = Add (wallPrefab, 1, 0);
+		if(right == null) right = Add (wallPrefab, 1, 180);
 
 		if(Random.value < 0.1f){
-			content = Add (chest, 2);
+			content = Add (chestPrefab, 2);
 		}
 	}
 }
